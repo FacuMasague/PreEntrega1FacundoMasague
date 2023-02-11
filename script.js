1d74862d778d696969a60d9d6c721c99e9bdc80a
Facundo Masague <Facu>
2023-02-10 21:35:17 -0300

    Arreglo casi completo del JavaScript, codigo mucho mas optimizado y lindo de leer

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let opc1 = prompt(`Buen dia ` + nombreDeUsuario + `, eliga una de las siguientes
 
 
 if (opc1 == "1") {
+
     function notaFinal(nota) {
         if (nota >= 7) {
             alert("Su nota es un " + nota + ", usted a aprobado")
@@ -21,101 +22,25 @@ if (opc1 == "1") {
             alert("Su nota es un " + nota + ", usted no a aprobado")
         }
     }
-    let opc2 = prompt(`Ha elegido calcular su nota del trimestre, porfavor escriba cuantas notas tuvo en el trimestre para realizar el calculo (Maximo 10)`)
 
-    if (opc2 == "2") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
+    let suma = 0
+    let division = true
 
-        let resultado = (num1+num2)/2
-        notaFinal(resultado)
-    } else if (opc2 == "3") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
+    while(division) {
+        division++
+        let sumaNueva = prompt(`Ingrese la nota
+        Si ha finalizado ingresando sus notas escriba "Listo"`)
 
-        let resultado = (num1+num2+num3)/3
-        notaFinal(resultado)
-    } else if (opc2 == "4") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-
-        let resultado = (num1+num2+num3+num4)/4
-        notaFinal(resultado)
-    } else if (opc2 == "5") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-
-        let resultado = (num1+num2+num3+num4+num5)/5
-        notaFinal(resultado)
-    } else if (opc2 == "6") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-        let num6 = Number(prompt("Escriba su sexta nota"))
-
-        let resultado = (num1+num2+num3+num4+num5+num6)/6
-        notaFinal(resultado)
-    } else if (opc2 == "7") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-        let num6 = Number(prompt("Escriba su sexta nota"))
-        let num7 = Number(prompt("Escriba su septima nota"))
-
-        let resultado = (num1+num2+num3+num4+num5+num6+num7)/7
-        notaFinal(resultado)
-    } else if (opc2 == "8") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-        let num6 = Number(prompt("Escriba su sexta nota"))
-        let num7 = Number(prompt("Escriba su septima nota"))
-        let num8 = Number(prompt("Escriba su octava nota"))
-
-        let resultado = (num1+num2+num3+num4+num5+num6+num7+num8)/8
-        notaFinal(resultado)
-    } else if (opc2 == "9") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-        let num6 = Number(prompt("Escriba su sexta nota"))
-        let num7 = Number(prompt("Escriba su septima nota"))
-        let num8 = Number(prompt("Escriba su octava nota"))
-        let num9 = Number(prompt("Escriba su novena nota"))
-
-        let resultado = (num1+num2+num3+num4+num5+num6+num7+num8+num9)/9
-        notaFinal(resultado)
-    } else if (opc2 == "10") {
-        let num1 = Number(prompt("Escriba su primer nota"))
-        let num2 = Number(prompt("Escriba su segunda nota"))
-        let num3 = Number(prompt("Escriba su tercer nota"))
-        let num4 = Number(prompt("Escriba su cuarta nota"))
-        let num5 = Number(prompt("Escriba su quinta nota"))
-        let num6 = Number(prompt("Escriba su sexta nota"))
-        let num7 = Number(prompt("Escriba su septima nota"))
-        let num8 = Number(prompt("Escriba su octava nota"))
-        let num9 = Number(prompt("Escriba su novena nota"))
-        let num10 = Number(prompt("Escriba su decima nota"))
-
-        let resultado = (num1+num2+num3+num4+num5+num6+num7+num8+num9+num10)/10
-        notaFinal(resultado)
-    } else {
-        alert("Esa cantidad de notas no es valida, asegurese de que sea entre dos y diez notas")
+        if (sumaNueva == "Listo") {
+            break
+        } else {
+            suma = suma + parseInt(sumaNueva)
+        }
     }
+    
+    let resultado = suma / (division - 2)
+    notaFinal(resultado)
+
 } else if (opc1 == "2") {
     let rangoNotas = 0
     for(rangoNotas ; rangoNotas <= 10 ; rangoNotas++) {
